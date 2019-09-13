6679002bf74118f831da137b2f136659a72cd8b8
Sergey Petrosyan <ipetriez@icloud.com>
2019-09-14 00:40:23 +0300

    Added PlayingCardView with an extension.

---
+++ PlayingCard/PlayingCardView.swift
@@ -0,0 +1,83 @@
+//
+//  PlayingCardView.swift
+//  PlayingCard
+//
+//  Created by Sergey Petrosyan on 9/12/19.
+//  Copyright © 2019 Sergey Petrosyan. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    var rank = 5 {
+        didSet {
+            setNeedsDisplay()
+            setNeedsLayout()
+        }
+    }
+    var suit = "❤️" {
+        didSet {
+            setNeedsDisplay()
+            setNeedsLayout()
+        }
+    }
+    var isFaceUp = true {
+        didSet {
+            setNeedsDisplay()
+            setNeedsLayout()
+        }
+    }
+    private func centeredAttributedString(_ string: String, fontSize: CGFloat) -> NSAttributedString {
+        var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
+        font = UIFontMetrics(forTextStyle: .body).scaledFont(for: font)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        return NSAttributedString(string: string, attributes: [.paragraphStyle: paragraphStyle, .font: font])
+    }
+    
+    private var cornerString: NSAttributedString {
+        return centeredAttributedString(rankString+"\n"suit, fontSize: 0.0)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: 16.0)
+        roundedRect.addClip()
+        UIColor.white.setFill()
+        roundedRect.fill()
+    }
+}
+
+extension PlayingCardView {
+    private struct SizeRatio {
+        static let cornerFontSizeToBoundsHeight: CGFloat = 0.085
+        static let cornerRadiusToBoundsHeight: CGFloat = 0.06
+        static let cornerOffsetToCornerRadius: CGFloat = 0.33
+        static let faceCardImageSizeToBoundsSize: CGFloat = 0.75
+    }
+    private var cornerRadius: CGFloat {
+        return bounds.size.height * SizeRatio.cornerRadiusToBoundsHeight
+    }
+    private var cornerOffset: CGFloat {
+        return cornerRadius * SizeRatio.cornerOffsetToCornerRadius
+    }
+    private var cornerFontSize: CGFloat {
+        return bounds.size.height * SizeRatio.cornerFontSizeToBoundsHeight
+    }
+    private var rankString: String {
+        switch rank {
+        case 1:
+            return "A"
+        case 2...10:
+            return String(rank)
+        case 11:
+            return "J"
+        case 12:
+            return "Q"
+        case 13:
+            return "K"
+        default:
+            return "?"
+        }
+    }
+}
