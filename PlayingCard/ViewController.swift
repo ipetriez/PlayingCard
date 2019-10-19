38214b1befe7f18feed932c550424b65d57c814f
Sergey Petrosyan <ipetriez@icloud.com>
2019-10-19 16:33:25 +0300

    Temporary commit and push.

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     var deck = PlayingCardDeck()
+    @IBOutlet var cardViews: [PlayingCardView]!
     
     
     @IBOutlet weak var playingCardView: PlayingCardView! {
@@ -23,15 +24,6 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
-        switch sender.state {
-        case .ended:
-            playingCardView.isFaceUp = !playingCardView.isFaceUp
-        default:
-            break
-        }
-    }
-    
     @objc func nextCard() {
         if let card = deck.draw() {
             playingCardView.rank = card.rank.order
@@ -41,9 +33,32 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        var cards = [PlayingCard]()
+        for _ in 1...((cardViews.count + 1) / 2) {
+            let card = deck.draw()!
+            cards += [card, card]
+        }
+        for cardView in cardViews {
+            cardView.isFaceUp = false
+            let card = cards.remove(at: cards.count.arc4random)
+            cardView.rank = card.rank.order
+            cardView.suit = card.suit.rawValue
+            cardView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(flipCard(_:))))
+        }
     }
-
-
+    
+    
+    
+    @objc func flipCard(_ recognizer: UITapGestureRecognizer) {
+//        switch recognizer.state {
+//        case .ended:
+//            if let chosenCardView = recognizer.view as? PlayingCardView {
+//                UIView.transition(with: chosenCardView, duration: 0.6, options: .transitionFlipFromLeft, animations: chosenCardView.isFaceUp = !chosenCardView.isFaceUp, completion: {finished in UIView.transition(with: cardView, duration: 0.6, options: .transitionFlipFromLeft, animations: cardView.isFaceUp = false)})
+//
+//            }
+//        default:
+//            break
+//        }
+    }
 }
 
